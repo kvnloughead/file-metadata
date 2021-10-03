8d15b5910d7e2a214512d41cd2d01f525202881c
Kevin Loughead <kvnloughead@gmail.com>
2021-10-03 14:55:28 -0400

    feat: add file-analysis route

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/'});
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,10 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname: name, mimetype: type, size} = req.file;
+  res.send({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
